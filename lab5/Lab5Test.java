0dec36a99486535d36faf3b4580c4b80fcbb0d65
Abdul78666 <121519218+Abdul78666@users.noreply.github.com>
2024-10-27 17:12:49 -0400

    Update Lab5Test.java

--- lab5/Lab5Test.java
+++ lab5/Lab5Test.java
@@ -12,44 +12,21 @@ import java.util.List;
 import java.util.ArrayList;
 class Lab5Test {
 	@Test 
-	void testInvalidLength(){
-		try {
-			Triangle T = new Triangle(-3,5);
-			assertFalse(true);
-		}catch(IllegalArgumentException ex) {
-			assertEquals(ex.getMessage(),"You crazy person! Length must be positive!");
-		}
-	}
-	@Test
-	void testInvalidWidth(){
-	try {
-		Triangle T = new Triangle(2,-5);
-		assertFalse(true);
-	}catch(IllegalArgumentException ex) {
-		assertEquals("You nutjob! Width must be positive!",ex.getMessage());
-	}
-}
-	@Test
-	void testCalculateArea() {
-		Triangle T = new Triangle(2,5);
-	    assertEquals(10, T.calculateArea()); 
+	void testCalculateArea(){
+			Point A = new Point(0,0);
+			Point B = new Point(0,4);
+			Point C = new Point(3,0);
+			Point D = new Point(1,2);
+			Rectangle R = new Rectangle(A,B,C,D);
+	    assertEquals(12, R.area()); 
 	}
 	@Test
 	void testCalculatePerimeter() {
-		Triangle T = new Triangle(2,6);
-	    assertEquals(16, T.calculatePerimeter()); 
-	}
-
-	@Test
-	void testGetLength() {
-		Triangle T = new Triangle(7,6);
-	    assertEquals(7, T.getLength()); 
-	}
-
-	@Test
-	void testGetWidth() {
-		Triangle T = new Triangle(7,4);
-	    assertEquals(4, T.getWidth()); 
-	}
-
+		Point A = new Point(0,0);
+		Point B = new Point(0,4);
+		Point C = new Point(3,0);
+		Point D = new Point(1,2);
+		Rectangle R = new Rectangle(A,B,C,D);
+    assertEquals(14, R.perimeter()); 
+    }
 }
