6d29eb22ea075e73c28f7e58e973d7c2c1e958b3
Abdul78666 <121519218+Abdul78666@users.noreply.github.com>
2024-10-27 17:11:36 -0400

    Create Lab5Test.java

---
+++ lab5/Lab5Test.java
@@ -0,0 +1,32 @@
+package com.example.setup_junit_eclipse_02;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+
+import org.junit.jupiter.api.Assertions;
+import java.util.List;
+import java.util.ArrayList;
+class Lab5Test {
+	@Test 
+	void testCalculateArea(){
+			Point A = new Point(0,0);
+			Point B = new Point(0,4);
+			Point C = new Point(3,0);
+			Point D = new Point(1,2);
+			Rectangle R = new Rectangle(A,B,C,D);
+	    assertEquals(12, R.area()); 
+	}
+	@Test
+	void testCalculatePerimeter() {
+		Point A = new Point(0,0);
+		Point B = new Point(0,4);
+		Point C = new Point(3,0);
+		Point D = new Point(1,2);
+		Rectangle R = new Rectangle(A,B,C,D);
+    assertEquals(14, R.perimeter()); 
+    }
+}
