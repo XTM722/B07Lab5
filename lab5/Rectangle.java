3c22cee1f2069ea9e58fa21f12a4da228c01274f
DrShettyy <krishang.shetty@mail.utoronto.ca>
2024-10-27 19:37:14 -0400

    Updated Rectangle.java to include exception error handling

--- lab5/Rectangle.java
+++ lab5/Rectangle.java
@@ -1,27 +1,36 @@
 package lab5;
 
-import javax.xml.namespace.QName;
-
 public class Rectangle {
-	Point A;
-	Point B;
-	Point C;
-	Point D;
-	
-	public Rectangle(Point A, Point B, Point C, Point D) {
-		this.A = A;
-		this.B = B;
-		this.C = C;
-		this.D = D;
-	}
-	
+    Point A;
+    Point B;
+    Point C;
+    Point D;
 
-	public double area() {
-		return A.distance(B)*A.distance(C);
-	}
+    public Rectangle(Point A, Point B, Point C, Point D) {
+        if (!isValidRectangle(A, B, C, D)) {
+            throw new IllegalArgumentException("Go back to grade school. Not a valid rectangle!");
+        }
+        this.A = A;
+        this.B = B;
+        this.C = C;
+        this.D = D;
+    }
 
-	public double perimeter() {
-		return 2* (A.distance(B) + A.distance(C));
-	}
+    private boolean isValidRectangle(Point A, Point B, Point C, Point D) {
+        double AB = A.distance(B);
+        double BC = B.distance(C);
+        double CD = C.distance(D);
+        double DA = D.distance(A);
 
+        // Check whether length and width the same measurement !
+        return AB == CD && BC == DA;
+    }
+
+    public double area() {
+        return A.distance(B) * A.distance(C);
+    }
+
+    public double perimeter() {
+        return 2 * (A.distance(B) + A.distance(C));
+    }
 }
