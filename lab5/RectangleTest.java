18aec23fc0575668019b53665a2bb5fdeb393f95
kevin <kevintsinghua@gmail.com>
2024-10-27 20:51:18 -0400

    Fix: Typo

--- lab5/RectangleTest.java
+++ lab5/RectangleTest.java
@@ -10,23 +10,23 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 import java.util.List;
 import java.util.ArrayList;
-class Lab5Test {
+class RectangleTest {
 	@Test 
 	void testCalculateArea(){
 			Point A = new Point(0,0);
 			Point B = new Point(0,4);
-			Point C = new Point(3,0);
-			Point D = new Point(1,2);
+			Point C = new Point(5,0);
+			Point D = new Point(5,4);
 			Rectangle R = new Rectangle(A,B,C,D);
-	    assertEquals(12, R.area()); 
+	    assertEquals(20, R.area()); 
 	}
 	@Test
 	void testCalculatePerimeter() {
 		Point A = new Point(0,0);
 		Point B = new Point(0,4);
-		Point C = new Point(3,0);
-		Point D = new Point(1,2);
+		Point C = new Point(4,0);
+		Point D = new Point(4,4);
 		Rectangle R = new Rectangle(A,B,C,D);
-    assertEquals(14, R.perimeter()); 
+    assertEquals(16, R.perimeter()); 
     }
 }
